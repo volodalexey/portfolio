df1608ccf530348ec40e038fcedbf2908765c34f
Alexey <volodalexey@gmail.com>
2023-06-01 17:17:47 +0300

    Ignore dist folder in main branch

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
-import path from 'node:path'
+import { join, resolve } from 'node:path'
+import { readdir, rm } from 'node:fs/promises'
 import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   base: '',
-  root: path.join(__dirname, "src"),
+  root: join(__dirname, "src"),
   build: {
-    outDir: path.join(__dirname, "dist"),
-    emptyOutDir: true
+    outDir: join(__dirname, "dist")
   },
   server: {
     port: 9120,
     host: true
   },
   plugins: [
+    {
+      name: "Cleaning dist folder",
+      async buildStart() {
+        const filePaths = await readdir(join(__dirname, 'dist'))
+        for (const filePath of filePaths) {
+          if (filePath.startsWith('.git')) {
+            continue
+          }
+          await rm(filePath, { force: true, recursive: true })
+        }
+      }
+    },
     ViteMinifyPlugin({}),
   ],
 })
